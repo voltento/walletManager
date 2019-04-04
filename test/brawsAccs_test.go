2fa969bf6ec0ead79099031ab3f11beec3979380
vladimir.dumanovskiy <vladimir.dumanovskiy@iqoption.com>
2019-04-04 22:48:31 +0300

    Add browsing accounts test.

---
+++ test/brawsAccs_test.go
@@ -0,0 +1,71 @@
+package test
+
+import (
+	"errors"
+	"fmt"
+	"github.com/voltento/walletManager/accmamaging"
+	"math/rand"
+	"reflect"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestBrawsAccount(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	ac := accmamaging.Account{
+		Id:       "test_" + strconv.Itoa(rand.Intn(100000)),
+		Currency: "USD",
+		Amount:   10,
+	}
+
+	er := addAccount(ac)
+	if er != nil {
+		t.Error(er.Error())
+	}
+
+	type args struct {
+		body string
+	}
+	tests := []struct {
+		name    string
+		want    CheckResp
+		wantErr bool
+	}{
+		{
+			name: "Browsing account: ok",
+			want: func(r httpResp) error {
+				expectedCode := 200
+				if r.code != expectedCode {
+					return errors.New(fmt.Sprintf("Got the unexpected return code. Expected: `%v` got: `%v`", expectedCode, r.code))
+				}
+
+				gotAcc, er := getAccount(ac.Id)
+				if er != nil {
+					return er
+				}
+
+				if !reflect.DeepEqual(ac, gotAcc) {
+					return errors.New(fmt.Sprintf("Got the unexpected account. Expected: `%v` got: `%v`", ac, gotAcc))
+				}
+				return nil
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := sendRequest(getAccountsUrl, "GET", "")
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("sendRequest() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if err = tt.want(got); err != nil {
+				t.Error(err.Error())
+			}
+		})
+	}
+}
